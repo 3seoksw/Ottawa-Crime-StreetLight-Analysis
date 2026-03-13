488229964a60a97fa67196f1f379fd5c5f24cef6
WooSeok Kim <3suksw@gmail.com>
2026-03-13 18:06:13 -0400

    update: remove Transformer attention module to simplify the model

--- src/model/attn_model.py
+++ src/model/attn_model.py
@@ -7,39 +7,23 @@ class AttentionModel(nn.Module):
     def __init__(
         self,
         n_features: int = 14,
-        k: int = 3,
         d_model: int = 128,
         n_heads: int = 4,
-        dim_feedforward: int = 256,
-        n_layers: int = 4,
     ):
         super().__init__()
-        # Equal to the window size + 2 (history + current + feature)
-        self.seq_len = k + 2
-
         self.feat_attn = FeatureAttention(d_model, n_heads)
         self.input = nn.Sequential(
-            nn.Linear(n_features, d_model),
+            nn.Linear(d_model, d_model),
             nn.LayerNorm(d_model),
             nn.GELU(),
         )
-        self.pos_emb = nn.Parameter(torch.randn(1, self.seq_len, d_model) * 0.02)
-
-        encoder_layer = nn.TransformerEncoderLayer(
-            d_model=d_model,
-            nhead=n_heads,
-            dim_feedforward=dim_feedforward,
-            batch_first=True,
-            norm_first=False,
-            activation="gelu",
-        )
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
+        self.embedding = nn.Parameter(torch.randn(1, n_features, d_model) * 0.02)
 
         # Shared output head
         self.output = nn.Sequential(
             nn.Flatten(),
-            nn.LayerNorm(d_model * self.seq_len),
-            nn.Linear(d_model * self.seq_len, d_model),
+            nn.LayerNorm(d_model * n_features),
+            nn.Linear(d_model * n_features, d_model),
             nn.GELU(),
             nn.Linear(d_model, d_model // 2),
         )
@@ -48,34 +32,26 @@ class AttentionModel(nn.Module):
         # If the classification results in nonzero, outputs crime count
         self.output_count = nn.Linear(d_model // 2, 1)
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor):
-        current = x[:, -1, :]
-        feat_attn = self.feat_attn(current)
-        # NOTE: Might be changed to GlobalPooling
-        feat_attn = feat_attn.mean(dim=1, keepdim=True)
+    def forward(self, x: torch.Tensor):
+        feat_attn, attn_weights = self.feat_attn(x)  # (B, 14, 128) & (B, 4, 14, 14)
 
-        h = self.input(x)
-        h_cat = torch.concat([h, feat_attn], dim=1)
-        h_pos = h_cat + self.pos_emb
+        h = self.input(feat_attn)
+        h_emb = h + self.embedding
 
-        feat_mask = torch.ones(mask.shape[0], 1, dtype=torch.bool).to(mask.device)
-        mask = torch.concat([mask, feat_mask], dim=1)
-        encoder = self.encoder(h_pos, src_key_padding_mask=~mask)
-
-        output = self.output(encoder)
+        output = self.output(h_emb)
         is_nonzero = self.output_cls(output).squeeze(1)
         count = self.output_count(output).squeeze(1)
-        return is_nonzero, count
+        return is_nonzero, count, attn_weights
 
-    def predict(self, x, mask):
+    def predict(self, x):
         self.eval()
         with torch.no_grad():
-            is_nonzero, count = self.forward(x, mask)
-        return is_nonzero, count
+            is_nonzero, count, attn_weights = self.forward(x)
+        return is_nonzero, count, attn_weights
 
 
 if __name__ == "__main__":
     model = AttentionModel()
-    x = torch.randn((64, 4, 14), dtype=torch.float32)
-    mask = torch.ones((64, 4), dtype=torch.bool)
-    model(x, mask)
+    x = torch.randn((64, 14), dtype=torch.float32)
+    nonzero, count, attn_weights = model(x)
+    print(nonzero.shape, count.shape, attn_weights.shape)
