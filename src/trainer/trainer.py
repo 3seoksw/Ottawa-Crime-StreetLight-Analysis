79a9bc98a4950697d9eb442b1422b22dde331710
WooSeok Kim <3suksw@gmail.com>
2026-03-14 04:19:09 -0400

    add: test method and run through plotting methods

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -3,7 +3,9 @@ import torch.nn as nn
 import numpy as np
 from data_module.dataloader import AggDataLoader
 from model.attn_model import AttentionModel
+from trainer.utils import plot_confusion_matrix, plot_attention_heatmap
 from torch.utils.tensorboard import SummaryWriter
+from sklearn.metrics import confusion_matrix
 
 
 class Trainer:
@@ -12,7 +14,8 @@ class Trainer:
         dataloader: AggDataLoader,
         model: AttentionModel,
         lr: float = 1e-3,
-        eval_every: int = 20,
+        log_every: int = 50,
+        eval_every: int = 100,
         log_dir: str = "runs/",
         device: str = "cpu",
         count_loss_type: str = "Poisson",
@@ -31,7 +34,9 @@ class Trainer:
         self.count_loss_type = count_loss_type
         self._assign_loss_functions(count_loss_type)
 
+        self.log_every = log_every
         self.eval_every = eval_every
+        self.log_dir = log_dir
         self.writer = SummaryWriter(log_dir=log_dir)
         self.global_step = 0
 
@@ -46,7 +51,7 @@ class Trainer:
 
     def loss_fn(self, y: torch.Tensor, is_nonzero: torch.Tensor, y_pred: torch.Tensor):
         # Binary Cross Entropy Loss (nonzero or zero crime count)
-        y_cls = (y > 0).float()
+        y_cls = (y > 0).float()  # class_0 .. zero crime & class_1 .. nonzero_crimes
         loss_cls = self.cls_loss_fn(is_nonzero, y_cls)
 
         # MSE or Poisson NLL Loss (crime count when nonzero)
@@ -63,30 +68,49 @@ class Trainer:
             loss_count = self.count_loss_fn(y_pred, y.float())
         return loss_cls, loss_count
 
+    def compute_accuracy(
+        self, y: torch.Tensor, is_nonzero: torch.Tensor, y_pred: torch.Tensor
+    ):
+        zero_mask = y == 0
+        nonzero_mask = y > 0
+
+        pred_cls = is_nonzero > 0
+
+        zero_correct = (pred_cls[zero_mask] == 0).sum()
+
+        pred_count = torch.round(y_pred)
+        nonzero_correct = (pred_count[nonzero_mask] == y[nonzero_mask]).sum()
+
+        correct = zero_correct + nonzero_correct
+        acc = correct.float() / y.numel()
+        return acc.item()
+
     def train_batch(self, batch: tuple[torch.Tensor, torch.Tensor]):
         self.optimizer.zero_grad()
         X, y = self._device_to(batch)
         is_nonzero, pred_y, _ = self.model(X)
+        acc = self.compute_accuracy(y, is_nonzero, pred_y)
         loss_cls, loss_count = self.loss_fn(y, is_nonzero, pred_y)
         loss = loss_cls + loss_count
         loss.backward()
         self.optimizer.step()
-        return loss_cls, loss_count
+        return loss_cls, loss_count, acc
 
-    def fit(self, max_epochs: int = 500, verbose: bool = False):
+    def fit(self, max_epochs: int = 5, verbose: bool = False):
         for e in range(max_epochs):
             for i, batch in enumerate(self.train_loader):
                 self.model.train()
-                loss_cls, loss_count = self.train_batch(batch)
-                if i % 20 == 0:
+                loss_cls, loss_count, acc = self.train_batch(batch)
+                if i % self.log_every == 0:
                     loss_cls = loss_cls.item()
                     loss_count = loss_count.item()
                     loss_sum = loss_cls + loss_count
-                    self.writer.add_scalar("loss/loss_cls", loss_cls, self.global_step)
+                    self.writer.add_scalar("train/loss_cls", loss_cls, self.global_step)
                     self.writer.add_scalar(
-                        "loss/loss_count", loss_count, self.global_step
+                        "train/loss_count", loss_count, self.global_step
                     )
-                    self.writer.add_scalar("loss/loss_sum", loss_sum, self.global_step)
+                    self.writer.add_scalar("train/loss_sum", loss_sum, self.global_step)
+                    self.writer.add_scalar("train/acc", acc, self.global_step)
                     if verbose:
                         print(f" [Epoch {e} - {i}] {self.global_step}")
                         print(f"  loss_cls: {loss_cls:.3f}")
@@ -104,19 +128,78 @@ class Trainer:
         self.model.eval()
         losses_cls = []
         losses_count = []
+        accuracies = []
         with torch.no_grad():
             for batch in self.val_loader:
                 X, y = self._device_to(batch)
                 is_nonzero, count, _ = self.model.predict(X)
+                acc = self.compute_accuracy(y, is_nonzero, count)
                 loss_cls, loss_count = self.loss_fn(y, is_nonzero, count)
+
                 losses_cls.append(loss_cls.item())
                 losses_count.append(loss_count.item())
+                accuracies.append(acc)
 
         avg_loss_cls = np.mean(losses_cls)
         avg_loss_count = np.mean(losses_count)
+        avg_accuracy = np.mean(accuracies)
+
         self.writer.add_scalar("val/loss_cls", avg_loss_cls, self.global_step)
         self.writer.add_scalar("val/loss_count", avg_loss_count, self.global_step)
+        self.writer.add_scalar("val/acc", avg_accuracy, self.global_step)
         return avg_loss_cls, avg_loss_count
+
+    def test(self):
+        self.model.eval()
+        trues_list = []
+        preds_list = []
+        attn_weights_list = []
+        accuracies = []
+        with torch.no_grad():
+            for i, batch in enumerate(self.test_loader):
+                X, y = self._device_to(batch)
+                is_nonzero, count, attn_weights = self.model.predict(X)
+                acc = self.compute_accuracy(y, is_nonzero, count)
+
+                attn_weights_list.append(attn_weights)
+                accuracies.append(acc)
+
+                # Loss
+                loss_cls, loss_count = self.loss_fn(y, is_nonzero, count)
+                loss_cls = loss_cls.item()
+                loss_count = loss_count.item()
+                loss_sum = loss_cls + loss_count
+                if i % 10 == 0:
+                    self.writer.add_scalar("test/loss_cls", loss_cls, i)
+                    self.writer.add_scalar("test/loss_count", loss_count, i)
+                    self.writer.add_scalar("test/loss_sum", loss_sum, i)
+                    self.writer.add_scalar("test/acc", acc, i)
+
+                # Confusion Matrix
+                probs = torch.sigmoid(is_nonzero)
+                pred = (probs > 0.5).int()
+                preds_list.append(pred)
+
+                true = (y > 0).int()
+                trues_list.append(true)
+
+            # Attention Map
+            attn_weights = torch.cat(attn_weights_list, dim=0)
+            attn_weights_per_head = attn_weights.mean(dim=0)
+            attn_weights_per_head = attn_weights_per_head.cpu().numpy()
+
+        trues = torch.cat(trues_list).cpu().numpy()
+        preds = torch.cat(preds_list).cpu().numpy()
+        cm = confusion_matrix(trues, preds)
+
+        plot_confusion_matrix(cm, True, self.log_dir)
+        plot_confusion_matrix(cm, False, self.log_dir)
+        plot_attention_heatmap(
+            attn_weights_per_head, self.dataloader.features, self.log_dir
+        )
+
+        self.writer.close()
+        return cm, attn_weights_per_head
 
     def _device_to(self, batch: tuple[torch.Tensor, torch.Tensor]):
         X, y = batch
